10f20a35482be887566c371e40e15301164b35a5
keepdev0919 <keepdev0919@example.com>
2025-12-11 19:45:45 +0900

    Remove: mws-assign-common02 폴더 삭제

--- mws-assign-common02/PhotoBlogServer/blog/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Post
-
-class PostSerializer(serializers.ModelSerializer):
-    image = serializers.ImageField(use_url=True, required=False)
-
-    class Meta:
-        model = Post
-        fields = ['id', 'title', 'text', 'created_date', 'published_date', 'image']
